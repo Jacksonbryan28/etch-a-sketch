f6ca8ea4479e4a3a78d5d21922c6773773814292
Jackson <jacksonbryan280@gmail.com>
2025-06-09 19:09:20 -0400

    added 16 containers using JS

--- javascript.js
+++ javascript.js
@@ -1,2 +1,8 @@
 const container = document.querySelector("#container");
 container.style.backgroundColor = "pink";
+
+for (i = 0; i < 16; i++) {
+  const div = document.createElement("div");
+  div.textContent = `${i}`;
+  container.appendChild(div);
+}
