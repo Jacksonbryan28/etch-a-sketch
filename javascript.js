224034217752c0fccb52953d921f4761978a3bea
Jackson <jacksonbryan280@gmail.com>
2025-06-09 19:25:03 -0400

    created a grid with 16x16 divs inside

--- javascript.js
+++ javascript.js
@@ -4,5 +4,11 @@ container.style.backgroundColor = "pink";
 for (i = 0; i < 16; i++) {
   const div = document.createElement("div");
   div.textContent = `${i}`;
+  //   div.classList.toggle("square");
   container.appendChild(div);
+  for (r = 0; r < 16; r++) {
+    const div = document.createElement("div");
+    div.textContent = "R:" + `${r}`;
+    container.appendChild(div);
+  }
 }
