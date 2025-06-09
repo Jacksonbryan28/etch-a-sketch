6cb73882ad8c846858d74fc066475711b1bffb33
Jackson <jacksonbryan280@gmail.com>
2025-06-09 19:35:48 -0400

    evenly spaced grid with fixed width

--- javascript.js
+++ javascript.js
@@ -4,11 +4,12 @@ container.style.backgroundColor = "pink";
 for (i = 0; i < 16; i++) {
   const div = document.createElement("div");
   div.textContent = `${i}`;
-  //   div.classList.toggle("square");
+  div.classList.toggle("square");
   container.appendChild(div);
-  for (r = 0; r < 16; r++) {
+  for (r = 1; r < 16; r++) {
     const div = document.createElement("div");
     div.textContent = "R:" + `${r}`;
+    div.classList.toggle("square");
     container.appendChild(div);
   }
 }
